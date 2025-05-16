f2eb82383d89db6d06b520991b7a69b5521ffc81
ChristianGit <santi.per.0620@gmail.com>
2025-05-16 18:38:49 -0500

    Modificando tf para admitir ecr existente

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,10 +1,5 @@
-resource "aws_ecr_repository" "app" {
-  name                 = var.ecr_repository_name
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "app" {
+  name = var.ecr_repository_name
 }
 
 resource "aws_ecs_cluster" "main" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "ecr_repository_url" {
   description = "URL of the ECR repository"
-  value       = aws_ecr_repository.app.repository_url
+  value       = data.aws_ecr_repository.app.repository_url
 }
 
 output "alb_dns_name" {
